adafecc92a5956a82dc3a5bb363da25a15f4c81c
yensei <julio.c.munhoz.p@gmail.com>
2022-02-19 20:38:16 -0300

    adiciona postman collection

--- src/main/java/py/com/mryensei/agile/agile/AgileApplication.java
+++ src/main/java/py/com/mryensei/agile/agile/AgileApplication.java
@@ -2,7 +2,6 @@ package py.com.mryensei.agile.agile;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class AgileApplication {
 
